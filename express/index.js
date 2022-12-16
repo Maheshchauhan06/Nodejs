5c1473c89608ce9b3ad698873302d8b612a3d363
M-Phoenixx <mchuhan66@gmail.com>
2022-12-16 12:30:47 +0530

    ejs learnt

--- express/index.js
+++ express/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+const newpath = path.join(__dirname,'public')
+ 
+// add html file
+// app.use(express.static(newpath));
 
-const newpath = path.join(__dirname, "getinghtml");
-app.use(express.static(newpath))
+app.set('view engine', 'ejs')
 
 app.get('/',(req,res)=>{
-    res.send('hlo')
+    const user = {
+        name :'mahesh',
+        sirname : 'chauhan',
+        skills : ['express','html','css']
+    }
+    res.render('index',{user})
 })
 
 app.listen(3000);
